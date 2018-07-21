ef0f9cb49cf87071ee8c53aae09991dd130cbcb4
liuspencersjtu <liusjtu@163.com>
2018-07-21 22:50:16 +0800

    modifying 450

--- 450-Delete-Node-in-a-BST.py
+++ 450-Delete-Node-in-a-BST.py
@@ -1,49 +1,47 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution(object):
-    def findmax(self, root):
-        if not root.right:
-            return root
-        else:
-            return self.findmax(root.right)
-        
-    def deletemax(self, root):
-        if not root.right:
-            return None
-        else:
-            root.right = self.deletemax(root.right)
-            return root
-        
+class Solution:
     def deleteNode(self, root, key):
         """
         :type root: TreeNode
         :type key: int
         :rtype: TreeNode
         """
-        if not root:
-            return None
-        if root.val == key:
-            if not root.left and not root.right:
-                root = None
+        p = root
+        king = TreeNode(0)
+        pupper = king
+        pupper.left = root
+        while True:
+            if not p:
                 return root
-            elif not root.left:
-                root = root.right
-                return root
+            if p.val == key:
+                break
+            elif p.val>key:
+                pupper = p
+                p = p.left
             else:
-                r = self.findmax(root.left)
-                root.val = r.val
-                #r = None
-                root.left = self.deletemax(root.left)
-                return root
-        # if still need searching
-        if root.val>key:
-            root.left = self.deleteNode(root.left,key)
+                pupper = p
+                p = p.right
+        if not p.left:
+            if pupper.left == p:
+                pupper.left = p.right
+            else:
+                pupper.right = p.right
         else:
-            root.right = self.deleteNode(root.right,key)
+            p2 = p.left
+            p3 = p
+            while (p2.right != None):
+                p3 = p2
+                p2 = p2.right
+            p.val = p2.val
+            if p3 != p:
+                p3.right = p2.left
+            else:
+                p3.left = p2.left
             
-        return root
+        return king.left
