5a14ef88d398fa72dff3935bb4c54311bc592118
liuspencersjtu <liusjtu@163.com>
2018-08-07 22:58:40 +0800

    adding 589

---
+++ 589-N-ary-Tree-Preorder-Traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        if not root:
+            return []
+        else:
+            ans = [root.val]
+            for item in root.children:
+                ans += self.preorder(item)
+            return ans
