fbddd866209503bc21e2ecdbd3834ccb66cfb831
liuspencersjtu <liusjtu@163.com>
2018-10-08 18:05:42 -0400

    adding 493

---
+++ 493-Reverse-Pairs.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reversePairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) <= 1:
+            return 0
+        count = [0]
+
+        def merge(nums):
+            if len(nums) <= 1: return nums
+            
+            left, right = merge(nums[:len(nums)//2]), merge(nums[len(nums)//2:])
+            l = r = 0
+            
+            while l < len(left) and r < len(right):
+                if left[l] <= 2 * right[r]:
+                    l += 1
+                else:
+                    count[0] += len(left) - l
+                    r += 1
+            return sorted(left+right)
+
+        merge(nums)
+        return count[0]
