4a813242e40f13e75baf71298310b689bae5bea1
liuspencersjtu <liusjtu@163.com>
2019-01-17 00:55:20 -0500

    adding 341

---
+++ 341-Flatten-Nested-List-Iterator.py
@@ -0,0 +1,61 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.lst = []
+        self.recurse(nestedList)
+        
+    def recurse(self,lst):
+        for item in lst:
+            if item.isInteger():
+                self.lst.append(item.getInteger())
+            else:
+                self.recurse(item.getList())
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        return self.lst.pop(0)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        if self.lst:
+            return True
+        return False
+    
+        
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
