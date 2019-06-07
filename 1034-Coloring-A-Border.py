a052e4c6baa5e30583c69a9d7c03ab3aee2d0d68
liuspencersjtu <liusjtu@163.com>
2019-06-06 23:55:50 -0700

    updating 1034

--- 1034-Coloring-A-Border.py
+++ 1034-Coloring-A-Border.py
@@ -12,3 +12,26 @@ class Solution:
             return True
         dfs(r0, c0)
         return grid
+
+
+
+###### BFS Solution
+import copy
+class Solution:
+    def colorBorder(self, grid: List[List[int]], r0: int, c0: int, color: int) -> List[List[int]]:
+        M,N = len(grid), len(grid[0])
+        visited = set([(r0,c0)])
+        border = set()
+        bfs = [[r0,c0]]
+        while bfs:
+            r,c = bfs.pop(0)
+            visited.add((r,c))
+            for a,b in [(1,0),(-1,0),(0,1),(0,-1)]:
+                if 0<=r+a<M and 0<=c+b<N and grid[r+a][c+b] == grid[r0][c0]:
+                    if (r+a,c+b) not in visited:
+                        bfs.append([r+a,c+b])
+                else:
+                    border.add((r,c))
+        for r,c in border:
+            grid[r][c]=color
+        return grid
