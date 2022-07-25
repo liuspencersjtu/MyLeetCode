294296def404a06e8b32f9c09a1e3b9d2d929957
liuspencersjtu <liusjtu@163.com>
2022-07-24 23:35:17 -0700

    adding 2354

---
+++ 2354-Number-of-Excellent-Pairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countExcellentPairs(self, nums: List[int], k: int) -> int:
+        hamming = [self.hammingWeight(num) for num in set(nums)]
+        hamming.sort()
+        ans = 0
+        for h in hamming:
+            ans += len(hamming) - bisect.bisect_left(hamming, k - h)
+        return ans
+        
+    def hammingWeight(self, n):
+        ans = 0
+        while n:
+            n &= (n - 1)
+            ans += 1
+        return ans
